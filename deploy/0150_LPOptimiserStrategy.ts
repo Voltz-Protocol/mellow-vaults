a75b36559f8f0cac45e4bc4eb5d3267ade2e99f0
tudor47 <tudorcostin7@gmail.com>
2022-11-24 22:03:41 +0200

    feat: usdt deployment

--- deploy/0150_LPOptimiserStrategy.ts
+++ deploy/0150_LPOptimiserStrategy.ts
@@ -6,7 +6,7 @@ import { constants } from "ethers";
 import { assert } from "console";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-    const kind = "DAI_31Dec22";
+    const kind = "USDT_31Mar23";
     const INSTANCE_NAME = `LPOptimiserStrategy-${kind}`;
 
     // Get the global deployment variables
@@ -16,12 +16,12 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         await getNamedAccounts();
 
     // Setup the parameters of the deployment
-    const VAULTS_TO_DEPLOY = 2;
+    const VAULTS_TO_DEPLOY = 1;
 
     // Get the tokens 
     // const tokens = [usdc].map((t) => t.toLowerCase()).sort();
-    // const tokens = [usdt].map((t) => t.toLowerCase()).sort();
-    const tokens = [dai].map((t) => t.toLowerCase()).sort();
+    const tokens = [usdt].map((t) => t.toLowerCase()).sort();
+    // const tokens = [dai].map((t) => t.toLowerCase()).sort();
     // const tokens = [weth].map((t) => t.toLowerCase()).sort();
 
     log("Tokens:", tokens);
@@ -35,16 +35,16 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     log();
 
     const marginEngines: {[key: string]: string} = {
-        'aUSDC_v3': '0xB785E7e71F099adA43222E1690Ee0bf701f80396',
-        'borrow_aUSDC_v1': '0x33bA6A0B16750206195c777879Edd8706204154B',
+        // 'aUSDC_v3': '0xB785E7e71F099adA43222E1690Ee0bf701f80396',
+        // 'borrow_aUSDC_v1': '0x33bA6A0B16750206195c777879Edd8706204154B',
         'borrow_cUSDT_v1': '0x111A75E91625142E85193b67B10E53Acf82838cD',
-        'aDAI_v3': '0x0F533F6b042593C00C9F4A2AD28106F524FCEb94',
-        'cDAI_v3': '0x75cDBD0e66Fdf4E2C80334F72B39628105fbeB20',
+        // 'aDAI_v3': '0x0F533F6b042593C00C9F4A2AD28106F524FCEb94',
+        // 'cDAI_v3': '0x75cDBD0e66Fdf4E2C80334F72B39628105fbeB20',
 
-        "stETH_v1": "0x21F9151d6e06f834751b614C2Ff40Fc28811B235",
-        "rETH_v1": "0xB1125ba5878cF3A843bE686c6c2486306f03E301",
-        "aETH_v1": "0x6F7ccb0cfD6130E75e88e4c72168fD8A6926c943",
-        "borrow_aETH_v1": "0x9b76B4d09229c339B049053F171BFB22cbE50092",
+        // "stETH_v1": "0x21F9151d6e06f834751b614C2Ff40Fc28811B235",
+        // "rETH_v1": "0xB1125ba5878cF3A843bE686c6c2486306f03E301",
+        // "aETH_v1": "0x6F7ccb0cfD6130E75e88e4c72168fD8A6926c943",
+        // "borrow_aETH_v1": "0x9b76B4d09229c339B049053F171BFB22cbE50092",
     }
 
     const vaultInitialParamDict: {[key: string]: {
@@ -53,61 +53,61 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         leverageWad: string,
         marginMultiplierPostUnwindWad: string,
     }} = {
-        'aUSDC_v3': {
-            tickLower: -7620,
-            tickUpper: 38820,
-            leverageWad: "50000000000000000000",
-            marginMultiplierPostUnwindWad: "2000000000000000000",
-        },
-        'borrow_aUSDC_v1': {
-            tickLower: -12060,
-            tickUpper: 300,
-            leverageWad: "50000000000000000000",
-            marginMultiplierPostUnwindWad: "2000000000000000000",
-        },
+        // 'aUSDC_v3': {
+        //     tickLower: -7620,
+        //     tickUpper: 38820,
+        //     leverageWad: "50000000000000000000",
+        //     marginMultiplierPostUnwindWad: "2000000000000000000",
+        // },
+        // 'borrow_aUSDC_v1': {
+        //     tickLower: -12060,
+        //     tickUpper: 300,
+        //     leverageWad: "50000000000000000000",
+        //     marginMultiplierPostUnwindWad: "2000000000000000000",
+        // },
         'borrow_cUSDT_v1': {
             tickLower: -15840,
             tickUpper: -7260,
             leverageWad: "50000000000000000000",
             marginMultiplierPostUnwindWad: "2000000000000000000",
         },
-        'aDAI_v3': {
-            tickLower: -7500,
-            tickUpper: 2100,
-            leverageWad: "50000000000000000000",
-            marginMultiplierPostUnwindWad: "2000000000000000000",
-        },
-        'cDAI_v3': {
-            tickLower: -7380,
-            tickUpper: 7500,
-            leverageWad: "50000000000000000000",
-            marginMultiplierPostUnwindWad: "2000000000000000000",
-        },
+        // 'aDAI_v3': {
+        //     tickLower: -7500,
+        //     tickUpper: 2100,
+        //     leverageWad: "50000000000000000000",
+        //     marginMultiplierPostUnwindWad: "2000000000000000000",
+        // },
+        // 'cDAI_v3': {
+        //     tickLower: -7380,
+        //     tickUpper: 7500,
+        //     leverageWad: "50000000000000000000",
+        //     marginMultiplierPostUnwindWad: "2000000000000000000",
+        // },
 
-        'stETH_v1': {
-            tickLower: -19080,
-            tickUpper: -17400,
-            leverageWad: "50000000000000000000",
-            marginMultiplierPostUnwindWad: "2000000000000000000",
-        },
-        'rETH_v1': {
-            tickLower: -19020,
-            tickUpper: -14160,
-            leverageWad: "50000000000000000000",
-            marginMultiplierPostUnwindWad: "2000000000000000000",
-        },
-        'aETH_v1': {
-            tickLower: -9180,
-            tickUpper: 11820,
-            leverageWad: "50000000000000000000",
-            marginMultiplierPostUnwindWad: "2000000000000000000",
-        },
-        'borrow_aETH_v1': {
-            tickLower: -11940,
-            tickUpper: -7620,
-            leverageWad: "50000000000000000000",
-            marginMultiplierPostUnwindWad: "2000000000000000000",
-        },
+        // 'stETH_v1': {
+        //     tickLower: -19080,
+        //     tickUpper: -17400,
+        //     leverageWad: "50000000000000000000",
+        //     marginMultiplierPostUnwindWad: "2000000000000000000",
+        // },
+        // 'rETH_v1': {
+        //     tickLower: -19020,
+        //     tickUpper: -14160,
+        //     leverageWad: "50000000000000000000",
+        //     marginMultiplierPostUnwindWad: "2000000000000000000",
+        // },
+        // 'aETH_v1': {
+        //     tickLower: -9180,
+        //     tickUpper: 11820,
+        //     leverageWad: "50000000000000000000",
+        //     marginMultiplierPostUnwindWad: "2000000000000000000",
+        // },
+        // 'borrow_aETH_v1': {
+        //     tickLower: -11940,
+        //     tickUpper: -7620,
+        //     leverageWad: "50000000000000000000",
+        //     marginMultiplierPostUnwindWad: "2000000000000000000",
+        // },
     }
 
     const vaultStrategyParamDict: {[key: string]: {
@@ -116,80 +116,74 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         proximityWad: string;
         weight: string;
     }} = {
-        'aUSDC_v3': {
-            sigmaWad: "1059469974466510000",
-            maxPossibleLowerBoundWad: "10000000000000000000",
-            proximityWad: "14024637172194800",
-            weight: "100",
-        },
-        'borrow_aUSDC_v1': {
-            sigmaWad: "1183652801711350000",
-            maxPossibleLowerBoundWad: "10000000000000000000",
-            proximityWad: "27432723972240800",
-            weight: "100",
-        },
+        // 'aUSDC_v3': {
+        //     sigmaWad: "1059469974466510000",
+        //     maxPossibleLowerBoundWad: "10000000000000000000",
+        //     proximityWad: "14024637172194800",
+        //     weight: "100",
+        // },
+        // 'borrow_aUSDC_v1': {
+        //     sigmaWad: "1183652801711350000",
+        //     maxPossibleLowerBoundWad: "10000000000000000000",
+        //     proximityWad: "27432723972240800",
+        //     weight: "100",
+        // },
         'borrow_cUSDT_v1': {
             sigmaWad: "1403960951174310000",
             maxPossibleLowerBoundWad: "10000000000000000000",
             proximityWad: "77138374607844200",
             weight: "100",
         },
-        'aDAI_v3': {
-            sigmaWad: "654228686487046000",
-            maxPossibleLowerBoundWad: "10000000000000000000",
-            proximityWad: "10507487663232100",
-            weight: "85",
-        },
-        'cDAI_v3': {
-            sigmaWad: "807771568062656000",
-            maxPossibleLowerBoundWad: "10000000000000000000",
-            proximityWad: "99994889768137400",
-            weight: "15",
-        },
+        // 'aDAI_v3': {
+        //     sigmaWad: "654228686487046000",
+        //     maxPossibleLowerBoundWad: "10000000000000000000",
+        //     proximityWad: "10507487663232100",
+        //     weight: "85",
+        // },
+        // 'cDAI_v3': {
+        //     sigmaWad: "807771568062656000",
+        //     maxPossibleLowerBoundWad: "10000000000000000000",
+        //     proximityWad: "99994889768137400",
+        //     weight: "15",
+        // },
 
-        'stETH_v1': {
-            sigmaWad: "518332746913213000",
-            maxPossibleLowerBoundWad: "10000000000000000000",
-            proximityWad: "26886037100646100",
-            weight: "52",
-        },
-        'rETH_v1': {
-            sigmaWad: "1298028550120740000",
-            maxPossibleLowerBoundWad: "10000000000000000000",
-            proximityWad: "69087513217907900",
-            weight: "18",
-        },
-        'aETH_v1': {
-            sigmaWad: "1095114934880330000",
-            maxPossibleLowerBoundWad: "10000000000000000000",
-            proximityWad: "99669859268131800",
-            weight: "15",
-        },
-        'borrow_aETH_v1': {
-            sigmaWad: "579696599024643000",
-            maxPossibleLowerBoundWad: "10000000000000000000",
-            proximityWad: "99931443604533200",
-            weight: "15",
-        },
+        // 'stETH_v1': {
+        //     sigmaWad: "518332746913213000",
+        //     maxPossibleLowerBoundWad: "10000000000000000000",
+        //     proximityWad: "26886037100646100",
+        //     weight: "52",
+        // },
+        // 'rETH_v1': {
+        //     sigmaWad: "1298028550120740000",
+        //     maxPossibleLowerBoundWad: "10000000000000000000",
+        //     proximityWad: "69087513217907900",
+        //     weight: "18",
+        // },
+        // 'aETH_v1': {
+        //     sigmaWad: "1095114934880330000",
+        //     maxPossibleLowerBoundWad: "10000000000000000000",
+        //     proximityWad: "99669859268131800",
+        //     weight: "15",
+        // },
+        // 'borrow_aETH_v1': {
+        //     sigmaWad: "579696599024643000",
+        //     maxPossibleLowerBoundWad: "10000000000000000000",
+        //     proximityWad: "99931443604533200",
+        //     weight: "15",
+        // },
     }
 
-    const VAULT_CAP = "500000000000000000000000";
+    const VAULT_CAP = "250000000000";
 
     // Voltz vault parameters
     const voltzVaultParams = [[
         tokens, // tokens
         deployer, // 
-        marginEngines['aDAI_v3'], // margin engine
+        marginEngines['borrow_cUSDT_v1'], // margin engine
         voltzVaultHelper, // voltz vault helper singleton
-        vaultInitialParamDict['aDAI_v3'], // initial parameters
-    ],
-    [
-        tokens, // tokens
-        deployer, // 
-        marginEngines['cDAI_v3'], // margin engine
-        voltzVaultHelper, // voltz vault helper singleton
-        vaultInitialParamDict['cDAI_v3'], // initial parameters
+        vaultInitialParamDict['borrow_cUSDT_v1'], // initial parameters
     ]];
+
     // const voltzVaultParams = [[
     //     tokens, // tokens
     //     deployer, // 
@@ -219,8 +213,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     //     vaultInitialParamDict['borrow_aETH_v1'], // initial parameters
     // ]];
 
-    const vaultStrategyParams = [vaultStrategyParamDict['aDAI_v3'], vaultStrategyParamDict['cDAI_v3']];
-    // const vaultStrategyParams = [vaultStrategyParamDict['stETH_v1'], vaultStrategyParamDict['rETH_v1'], vaultStrategyParamDict['aETH_v1'], vaultStrategyParamDict['borrow_aETH_v1']];
+    const vaultStrategyParams = [vaultStrategyParamDict['borrow_cUSDT_v1']];
 
     assert(voltzVaultParams.length === VAULTS_TO_DEPLOY, "length doesn't match");
     assert(vaultStrategyParams.length === VAULTS_TO_DEPLOY, "length doesn't match");
